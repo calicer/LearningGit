b2fff3d727c1bd0a8fe7bb161e6ab25a415f5710
vishal bhalla <you@example.com>
2021-08-26 20:55:51 +0530

    added moified emp_test and new file git_test

--- src/emp_test.go
+++ src/emp_test.go
@@ -1,8 +1,12 @@
 package fd
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestCreateEmp(t *testing.T) {
 	err := CreateEmployee(Employee{id: "1", name: "Vishal", dept: "EAIS"})
 
+	fmt.Println("For Git")
 }
